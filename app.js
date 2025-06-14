dd1cc42412a326a4fb8cb37b17657901eda9e3b1
GreenCodeComments <pguichon1@bcit.ca>
2025-06-14 15:25:44 -0700

    Simple website playing with routes with URL query params and virtual folders

--- app.js
+++ app.js
@@ -6,8 +6,37 @@ const PORT = process.env.PORT || 3000;
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('<h1>Hello, World!</h1>');
+    res.send(`
+        <h1>Hello, World!</h1>
+        <a href="/about?color=blue">About (Blue)</a><br>
+        <a href="/about?color=orange">About (Orange)</a><br>
+        <a href="/cat/1">Cat 1</a><br>
+        <a href="/cat/2">Cat 2</a><br>
+        <a href="/cat/3">Cat 3?</a><br>`);
 });
+
+app.get('/about', (req,res) => {
+    var color = req.query.color;
+
+    res.send("<h1 style='color:"+color+";'>Patrick Guichon</h1>");
+});
+
+app.get('/cat/:id', (req,res) => {
+
+    var cat = req.params.id;
+
+    if (cat == 1) {
+        res.send("Fluffy: <img src='/fluffy.gif' style='width:250px;'>");
+    }
+    else if (cat == 2) {
+        res.send("Socks: <img src='/socks.gif' style='width:250px;'>");
+    }
+    else {
+        res.send("Invalid cat id: "+cat);
+    }
+});
+
+app.use(express.static(__dirname + "/public"));
 
 // Start server
 app.listen(PORT, () => {
